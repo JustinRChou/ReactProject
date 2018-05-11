14d892ca918d5c3725f2aa8397ea8eef8060c95c
JustinRChou <justinrchou@gmail.com>
2018-05-11 01:20:04 -0700

    Headings update

--- ReactProject/ClientApp/components/Headings.tsx
+++ ReactProject/ClientApp/components/Headings.tsx
@@ -4,32 +4,59 @@ export interface HeadingProps {
     data: Array<ChangeState>;
     title: string;
 };
+interface HeadingsProps {
+    heading: string;
+};
+interface Headings2Props {
+    headings: Array<string>;
+};
 interface ChangeState {
     when: string | undefined;
     who: string | undefined;
     description: string | undefined;
 };
+class Heading extends React.Component<HeadingsProps>{
+    public render() {
+        return (<th style={{ color: "red" }}>{this.props.heading}</th>);
+    }
+};
+
+class Headings2 extends React.Component<Headings2Props > {
+    public render() {
+    var headings2 = this.props.headings.map(function (heading) {
+        return (<Heading heading={heading} />);
+    });
+    return <thead><tr>{headings2}</tr></thead>;
+    }
+};
+class Row extends React.Component<{ changes: ChangeState }>{
+    public render() {
+        return (<tr>
+            <td> {this.props.changes.when} </td>
+            <td style={{ color: "green" }}> {this.props.changes.who} </td>
+            <td> {this.props.changes.description} </td>
+        </tr>);
+    }
+};
+
+class Row2 extends React.Component<{ changeSet: Array<ChangeState>}> {
+    public render() {
+        var rows = this.props.changeSet.map(function (change) {
+            return (<Row changes={change} />);
+        });
+        return <tbody>{rows}</tbody>;
+    }
+};
+    
 export class Headings extends React.Component<HeadingProps, ChangeState>{
     constructor() {
         super();
         this.state = { when: undefined, who: undefined, description: undefined };
     }
     public render() {
-    var headings = this.props.headings.map(function (heading:string) {
-        return (<th>
-            {heading}
-        </th>);
-    });
-    var rows = this.props.data.map(function (change: ChangeState) {
-        return (<tr>
-            <td> {change.when} </td>
-            <td> {change.who} </td>
-            <td> {change.description} </td>
-        </tr>);
-    });
         return (<div><h1>{this.props.title}</h1><table>
-        {headings}
-        {rows}
+            <Headings2 headings={this.props.headings}/>
+            <Row2 changeSet={this.props.data} />
         </table></div>);
-}
+    }
 }
